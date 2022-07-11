fc36e693584e0822ccd7e2f3d9a273e547cd38db
unknown <4friizal@gmail.com>
2022-07-11 22:48:17 +0700

    new project react

---
+++ src/component/Header.js
@@ -0,0 +1,35 @@
+import "./../index.css";
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="pl-7 md:py-10 bg-[#113050] flex justify-between items-center lg:py-3">
+        {/* <img
+          src={"https://picsum.photos/100"}
+          className="text-black w-8 h-8 md:w-10 rounded-full"
+          alt="Movies"
+        /> */}
+        <button className="hidden md:flex md:items-center md:text-3xl md:font-bold md:text-yellow-400 lg:text-base lg:ml-10">
+          Home
+        </button>
+        <button className="md:hidden outline-none mobile-menu-button">
+          <svg
+            className="w-8 h-8 text-[#9b9d9f]"
+            x-show="!showMenu"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/component/ListMovie.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./../index.css";
+
+class OptionFilm extends React.Component {
+  render() {
+    return (
+      <div className="lg:flex lg:justify-around">
+        <div>
+          <img
+            className="mx-auto md:mx-4 w-64 h-[23rem]  drop-shadow-lg pt-10 lg:w-44 lg:h-60"
+            src="https://picsum.photos/202"
+            alt="film"
+          />
+          <h1 className="text-center w-64 mx-auto py-4 font-semibold text-xl lg:text-xs lg:w-40">
+            Judul Film 1
+          </h1>
+        </div>
+        <div>
+          <img
+            className="mx-auto md:mx-4 w-64 h-[23rem]  drop-shadow-lg pt-10 lg:w-44 lg:h-60"
+            src="https://picsum.photos/200"
+            alt="film"
+          />
+          <h1 className="text-center w-64 mx-auto py-4 font-semibold text-xl lg:text-xs lg:w-40">
+            Judul Film 2
+          </h1>
+        </div>
+        <div>
+          <img
+            className="mx-auto md:mx-4 w-64 h-[23rem]  drop-shadow-lg pt-10 lg:w-44 lg:h-60"
+            src="https://picsum.photos/256"
+            alt="film"
+          />
+          <h1 className="text-center w-64 mx-auto py-4 font-semibold text-xl lg:text-xs lg:w-40">
+            Judul Film 3
+          </h1>
+        </div>
+        <div>
+          <img
+            className="mx-auto md:mx-4 w-64 h-[23rem]  drop-shadow-lg pt-10 lg:w-44 lg:h-60"
+            src="https://picsum.photos/257"
+            alt="film"
+          />
+          <h1 className="text-center w-64 mx-auto py-4 font-semibold text-xl lg:text-xs lg:w-40">
+            Judul Film 4
+          </h1>
+        </div>
+        <div>
+          <img
+            className="mx-auto md:mx-4 w-64 h-[23rem]  drop-shadow-lg pt-10 lg:w-44 lg:h-60"
+            src="https://picsum.photos/253"
+            alt="film"
+          />
+          <h1 className="text-center w-64 mx-auto py-4 font-semibold text-xl lg:text-xs lg:w-40">
+            Judul Film 5
+          </h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OptionFilm;
